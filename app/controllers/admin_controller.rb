52beeb004750d2e7b6d0e3022ae419f61eef90fb
Diego <diego.ma.martinez@gmail.com>
2016-03-28 17:27:41 -0400

    added crud to Users and admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,19 @@
 class AdminController < ApplicationController
-  def index
-  end
+  	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+	 
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
