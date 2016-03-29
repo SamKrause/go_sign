4fd0fe9f739114089bdff90d438c656a8938a5ba
Diego <diego.ma.martinez@gmail.com>
2016-03-29 13:38:50 -0400

    created home view, added devise login logout signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+root 'home#index'
+  get 'home' => 'home#index'
+  get 'about' => 'home#about'
+
+
   resources :users
   resources :admins
   resources :questions
   resources :categories
   resources :answers
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
