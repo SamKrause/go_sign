81e8ea4bb95a8e797a2bcb274232048dc4eac34f
Sam Krause <samuelnkrause@gmail.com>
2016-03-31 18:44:23 -0400

    finished javascript for looping through tests and connected it to success page and back to category page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   post '/sessions/login' => 'sessions#login'
 
   get '/tests/family' => 'tests#familyQuestions'
+  get '/tests/finished_test' => 'tests#finishedTestPage'
+  get '/tests/categories' => 'tests#categories'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
