0dc93acc8925557f493f3e3d09df1e9ac5ba4cb7
sharpattack <sharp1209@gmail.com>
2016-03-31 12:40:52 -0400

    added gon gem, played around with gon in tests controller and tests.js

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
   var question_array = [];
-  var $category_name = $('.category_name').html();
-  $(".family").click(function(){
-    
+  var $category_name = $('.category_name');
+  $category_name.click(function(){
+    if ($(this).text() == "Family"){
+      console.log("true, this is family");
+      var gfamily = gon.family_answers;
+      console.log(gfamily.length);
+      for(var i = 0; i < gfamily.length; i ++){
+        console.log(gfamily[i].name);
+      }
+      $('.question').html("<img src=\"gfamily[0].gif\">");
+    }
   });
 
 })
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
   def index
     @family = Category.find_by_name("Family")
+    gon.family_answers = @family.answers.each { |a| a.name }
     @categories = Category.all
     @answers = Answer.all
   end
