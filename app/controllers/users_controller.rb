04403e4123de61fe0e141e5203c777cd56e1f944
Sam Krause <samuelnkrause@gmail.com>
2016-03-31 12:04:52 -0400

    added ajax for log in and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def index
+  def signup
+    @user = User.create(user_params)
+    sign_in(:user, @user)
+
+    if @user.valid?
+      view_string = render_to_string partial: 'home/welcome'
+      render json: {success: true, welcome: view_string}
+    else
+      render json: {success: false, errors: @user.errors.full_messages}
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :first_name, :last_name, :avatar)
   end
 end
